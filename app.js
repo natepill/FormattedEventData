dcc676909363b23fe3ed450fa218ced3aa2b0e24
Nathan Pillai <natepill@gmail.com>
2019-03-12 14:33:19 -0700

    Initial integration of the startbootstrap-landing-page

--- app.js
+++ app.js
@@ -11,10 +11,14 @@ const bodyParser = require('body-parser');
 
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.use(require('method-override')('_method'));
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
