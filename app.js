83ff05c8092b45ff70ba824faf407abc92015bb3
Nathan Pillai <natepill@gmail.com>
2019-03-14 12:58:51 -0700

    ADDED form submit route that grabs form data and makes a POST request to our mircroservice

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
-
+const request = require('request');
+const axios = require('axios');
 
 // const fs = require('fs');
 // const request = require('request')
-
-
 
 const port = process.env.PORT || 5000;
 
@@ -23,6 +22,17 @@ app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/form-submit', (req, res) => {
+    const form_data = req.query
+
+    // TODO: Error handing w/ Axios
+    axios.post('http://127.0.0.1:5000/', {form_data: form_data });
+
+    // TODO: Redirect to User's Dashboard
+    // TODO: Look @ Ikey's Authentication middleware in MMYBO project NOTE: (MAY NOT WORK FOR ALL ROUTES) because we are trying to integrate a developer API
+    res.redirect('/');
 })
 
 
